76dd64ceafb4bc20b0c80a3b7f3be1a380dda704
Chris <crp5029@gmail.com>
2014-03-19 05:30:46 -0400

    Initial Commit

---
+++ source/ui/ios/StaticResources/StaticResources/StaticResources.h
@@ -0,0 +1,13 @@
+//
+//  StaticResources.h
+//  StaticResources
+//
+//  Created by Chris Purtell on 3/19/14.
+//  Copyright (c) 2014 Chris Purtell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StaticResources : NSObject
+
+@end
---
+++ source/ui/ios/StaticResources/StaticResources/StaticResources.m
@@ -0,0 +1,13 @@
+//
+//  StaticResources.m
+//  StaticResources
+//
+//  Created by Chris Purtell on 3/19/14.
+//  Copyright (c) 2014 Chris Purtell. All rights reserved.
+//
+
+#import "StaticResources.h"
+
+@implementation StaticResources
+
+@end
